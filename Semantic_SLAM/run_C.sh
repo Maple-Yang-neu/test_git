1af490976f08dc9c77e392a85f56f239f9d77874
ylh <yanglinghaoneu@163.com>
2020-08-04 12:54:13 +0800

    test2

--- Semantic_SLAM/run_C.sh
+++ Semantic_SLAM/run_C.sh
@@ -4,5 +4,4 @@ sleep 2
 gnome-terminal -x bash -c "cd Third_Part/PSPNet_Keras_tensorflow/
 python Semantic_Information_Publisher_C.py image:=/camera/image_raw"
 
-gnome-terminal -x bash -c " cd Third_Part/ORB_SLAM2/
-rosrun ORB_SLAM2 Mono Vocabulary/ORBvoc.txt Examples/Monocular/KITTI04-12.yaml"
+
