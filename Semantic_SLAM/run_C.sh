68866c758a2fbd65d17a2271108a17a9865570e6
ylh <yanglinghaoneu@163.com>
2020-08-05 09:14:44 +0800

    run_C.sh change

--- Semantic_SLAM/run_C.sh
+++ Semantic_SLAM/run_C.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gnome-terminal -x bash -c "roscore"
 sleep 2
-gnome-terminal -x bash -c "cd Third_Part/PSPNet_Keras_tensorflow/
-python Semantic_Information_Publisher_C.py image:=/camera/image_raw"
+
+
 
